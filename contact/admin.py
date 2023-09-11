74f1d3f240b6f326b046d0dfe812617891a21e82
devvictorh <victor.dev.henrique@gmail.com>
2023-09-10 21:31:21 -0300

    news models

--- contact/admin.py
+++ contact/admin.py
@@ -3,13 +3,18 @@ from django.contrib import admin
 from contact import models
 
 
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description')
+
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    list_display = ('id', 'first_name', 'last_name', 'category',)
     ordering = ('id', 'first_name')
     list_filter = ('created_date',)
     search_fields = ('id', 'first_name', 'last_name')
     list_per_page = 10
     list_max_show_all = 200
     # list_editable = ('first_name', 'last_name')
-    list_display_links = 'first_name',
+    list_display_links = ('first_name',)
--- contact/urls.py
+++ contact/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from contact.views import index
@@ -5,3 +7,7 @@ from contact.views import index
 urlpatterns = [
     path('', index, name='index'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
